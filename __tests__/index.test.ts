4ddb00c77968b994c21f9bef8b0920e1c0077083
Yogesh Joshi <joshiyogesh001@gmail.com>
2020-02-17 16:48:04 +0530

    Mocked the test cases for SoapRequest

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,15 +1,50 @@
 const fs = require('fs');
 const soaptestrequest = require('../src/index');
-const url = 'www.dneonline.com/calculator.asmx';
+const axiosTest = require('axios');
+const MockAdapter = require('axios-mock-adapter');
+const Mock = new MockAdapter(axiosTest);
+
+const url = 'http://www.dneonline.com/calculator.asmx';
+const failureURl = 'http://www.dneonline.com/failure/calculator.asmx'
 const data = fs.readFileSync('__tests__/_resource/request1.xml', 'utf-8');
+const responseData = fs.readFileSync('__tests__/_resource/response1.xml', 'utf-8');
+const failureData = fs.readFileSync('__tests__/_resource/request2.xml', 'utf-8');
+const failureResponseData = fs.readFileSync('__tests__/_resource/response2.xml', 'utf-8')
 const headers = {
     'Content-Type': `text/xml; charset=utf-8`,
     'Content-Length': Infinity,
-    SOAPAction: "http://tempuri.org/Add"
-}
+    'SOAPAction': "http://tempuri.org/Add"
+};
 test('soaprequestTest', async () => {
-    const { response } = await soaptestrequest({ url, data, headers });
-    console.log('Hello Yogesh')
-    console.log({ response })
-    expect(response).toEqual('');
-})
+    const axiosResponse = responseData;
+    Mock.onPost(url).reply(200, axiosResponse)
+    let { response } = await soaptestrequest({ url, data, headers });
+    expect(response.body).toEqual(responseData);
+    expect(response.status).toEqual(200);
+});
+
+test('soaprequestTest failure', async () => {
+    const failAxiosResponse = failureResponseData;
+    Mock.onPost(failureURl).reply(200, failAxiosResponse);
+    let response: any;
+    try {
+        response = await soaptestrequest({ url: failureURl, data: failureData, headers});
+    } catch (exception) {
+        console.log('service failure due to ', exception);
+    }
+    expect(response.status).not.toEqual(200);
+});
+
+test('soaprequestTest failure for timeout', async () => {
+    Mock.onPost(failureURl).timeout();
+    let response: any;
+    let status : any;
+    try {
+        response = await soaptestrequest({ url: failureURl, data: failureData, headers});
+        status = {response};
+    } catch (exception) {
+        //console.log('service failure due to ', exception);
+    }
+    expect(status).not.toEqual(200);
+});
+
--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,20 @@ const axios = require('axios').default;
  * @param {string} data - XML data which need to be sent to web service
  * @param {object} headers - Header which need to be sent to request
  * @param {object} proxy - Proxy Object with Proxy configuration of Proxy server host name and port number {host : "127.0.0.1",port : 3000, auth : {username : "joshiyogesh" password : "imduffer"}}
- * @param {number} timeout - Timeout for request
+ * @param {number} timeout - Specify the number of milliseconds before request timesout, if request take more than specify time out it will be aborted,
+ * timeout - 0 specify no timeout
  * @param {number} maxContentLength - max permissable content length that should be pass, by default Infinity 
+ * @param {object} extraOptions - Any Extra Options which is supported by axios. 
+ * 
+ * @return {object} -- {
+ *  response : {
+ *      message : "SOAP_WEB_CLIENT_SOAP_REQUEST_SUCCESS" or "SOAP_WEB_CLIENT_REQUEST_FAILURE",
+ *      data : "In case of Promise Rejection",
+ *      body : "In Case of Succesfull Promise",
+ *      headers : "In Case of Succesfull Promise",
+ *      "status" : "In Case of Succesfull Prmoise"
+ *  }
+ * }
  */
 const soaprequest = ({
     url = '',
@@ -14,8 +26,9 @@ const soaprequest = ({
     data = '',
     timeout = 0,
     proxy = {},
-    maxContentLength  = Infinity
-}:{url:string, headers:object,data:string,timeout:number,proxy:object,maxContentLength:number}) => {
+    maxContentLength  = Infinity,
+    extraOptions = {}
+}:{url:string,headers:object,data:string,timeout:number,proxy:object,maxContentLength:number,extraOptions:object}) => {
     return new Promise((resolve,reject)=>{
         axios({
             method : 'post',
@@ -24,27 +37,29 @@ const soaprequest = ({
             data,
             timeout,
             proxy,
-            maxContentLength
+            maxContentLength,
+            ...extraOptions
         }).then((response : any)=>{
             resolve({
-                res : {
+                response : {
                     headers : response.headers,
                     body : response.data,
-                    status : response.status
+                    status : response.status,
+                    message : 'SOAP_WEB_CLIENT_SOAP_REQUEST_SUCCESS'
                 }
             })
         }).catch((error : any)=>{
             if(error.response){
                 reject({
-                    error : {
-                        message : 'SOAP_REQUEST_FAILURE',
+                    response : {
+                        message : 'SOAP_WEB_CLIENT_SOAP_REQUEST_FAILURE',
                         data : error.response.data
                     }
                 })
             }else{
                 reject({
-                    error : {
-                        message : 'SOAP_REQUEST_FAILURE',
+                    response : {
+                        message : 'SOAP_WEB_CLIENT_REQUEST_FAILURE',
                         data : error
                     }
                 })
